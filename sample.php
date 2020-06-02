3d894323a596a05855f9d2767bb8f4fbf78a09ff
shuhei.tachikawa <shuhei.tusx@gmail.com>
2020-06-02 00:33:53 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果:無し</p>
+        <form>
+            <button type="submitt" name="act" value="draw">おみくじを引く！</button>
+        </form>
+    </body>
+</html>
